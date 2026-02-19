7adfdd0ad0eb23598c13ba496c617fbf14d4f975
SilverSpecter007 <julian.pape@gmail.com>
2026-02-19 21:32:30 +0100

    Remove base path from astro.config.mjs
    Removed base path configuration for non-root repo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://SilverSpecter007.github.io',
-  base: '/Birthday_Party', // nur wenn nicht das Root-Repo ist
   output: 'server',
   adapter: node({
     mode: 'standalone',
