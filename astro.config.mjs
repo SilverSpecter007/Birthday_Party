d928e113790652019bed08bc0fe607d82360f133
SilverSpecter007 <julian.pape@gmail.com>
2026-02-19 22:20:22 +0100

    Modify Vercel adapter import and set site URL
    Updated Vercel import to use serverless version and added site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://birthday-party-git-main-julianpape-7777s-projects.vercel.app',
   output: 'server',
   adapter: vercel(),
   vite: {
