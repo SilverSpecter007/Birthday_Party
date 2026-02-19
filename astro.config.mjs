6a1f6542949f5d48e4604aa8f7384ca121daed1d
SilverSpecter007 <julian.pape@gmail.com>
2026-02-19 22:12:08 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://deine-seite.vercel.app',
   output: 'server',
   adapter: vercel(),
   vite: {
